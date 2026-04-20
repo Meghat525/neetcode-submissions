585a8d8f9d5f2fe9c6d67d5d696fecef810ab7bb
Meghat525 <51199291+Meghat525@users.noreply.github.com>
2026-04-20 20:53:18 +0530

    Add: fruit-into-baskets - submission-3

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-3.py
@@ -0,0 +1,19 @@
+# [1,0,1,4,1,4,1,2,3]
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        max_len = 0
+        l = r = 0
+        hashmap = {}
+        while r < len(fruits):
+            hashmap[fruits[r]] = hashmap.get(fruits[r], 0) + 1
+            while len(hashmap) > 2:
+                left_fruit = fruits[l]
+                hashmap[left_fruit] -= 1
+                if hashmap[left_fruit] == 0:
+                    del hashmap[left_fruit]
+                l += 1
+            
+            max_len = max(r - l + 1, max_len)
+            r += 1
+            
+        return max_len
