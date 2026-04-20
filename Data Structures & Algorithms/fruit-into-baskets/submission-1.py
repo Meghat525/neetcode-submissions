63e2e2142ea7a471903d0e56b9bddae39e48351c
Meghat525 <51199291+Meghat525@users.noreply.github.com>
2026-04-20 20:52:13 +0530

    Add: fruit-into-baskets - submission-1

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-1.py
@@ -0,0 +1,24 @@
+# [1,0,1,4,1,4,1,2,3]
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        max_len = 0
+        l = r = 0
+        hashmap = {}
+        while r < len(fruits):
+            hashmap[fruits[r]] = hashmap.get(fruits[r], 0) + 1
+            while len(hashmap) > 2:
+                left_fruit = fruits[l]
+                hashmap[left_fruit] -= 1
+                
+                # If a fruit type is completely gone from the window, remove it from the map
+                if hashmap[left_fruit] == 0:
+                    del hashmap[left_fruit]
+                
+                # Shrink the window
+                l += 1
+            
+            max_len = max(r - l + 1, max_len)
+            print(r - l + 1, max_len, l, r)
+            r += 1
+            
+        return max_len
